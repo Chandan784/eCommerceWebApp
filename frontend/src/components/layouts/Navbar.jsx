7fbfa19ed494417812e22a3e7f9700775b5443a4
benjimanraj <benjimanraj@outlook.com>
2025-05-22 16:42:05 +0530

    nav bar items added

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 function Navbar() {
-  return <div style={{height:"10vh",backgroundColor:"blue"}}>nabvar
-  <h1 className="italic">navbar</h1>
-  </div>;
+  return (
+    <div style={{ height: "10vh", 
+    backgroundColor: "blue",
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center",
+    color:"white",
+    padding:"30px",
+    fontSize:"30px" }}>
+      <div className="left">
+        <h1 className="">Macramee</h1>
+      </div>
+      <div className="right p-8">
+        <a href="/home" className="p-8">Home</a>
+        <a href="/about"className="p-8">About</a>
+        <a href="/contact"className="p-8">Contact</a>
+        <a href="/service"className="p-8">Service</a>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
