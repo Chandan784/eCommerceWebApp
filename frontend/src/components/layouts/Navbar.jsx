2b7be2c460c38baa2a66ed1811b509a88dd99442
Harika sutharapu <harikanivas2531@gmail.com>
2025-05-22 16:17:31 +0530

    navbar under progress

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Navbar() {
-  return <div>nabvar</div>;
+  return <div style={{height:"10vh",backgroundColor:"blue"}}>nabvar
+  <h1 className="italic">navbar</h1>
+  </div>;
 }
 
 export default Navbar;
